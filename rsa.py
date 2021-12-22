9fdab9c73b8b3bdd1ee4c88f54d475dbe7fb2f76
Rana Aahil <85215747+rsaprogrammers@users.noreply.github.com>
2021-12-22 22:15:29 +0500

    Update rsa.py

--- rsa.py
+++ rsa.py
@@ -27,8 +27,8 @@ except ImportError:
 
 rana=platform.architecture()[0]
 if rana=="32bit":
-    import chotarsa
-    chotarsa.start()
+    import minirsa
+    minirsa.main_system()
 elif rana=="64bit":
     import rsa64bit
     rsa64bit.main_system()
