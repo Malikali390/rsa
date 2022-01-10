d2330ada824bc66b8ff3543e06ccecf53e4bf082
Rana Aahil <85215747+rsaprogrammers@users.noreply.github.com>
2022-01-11 00:33:48 +0500

    Update rsa.py

--- rsa.py
+++ rsa.py
@@ -16,11 +16,6 @@ try:
 except ImportError:
     os.system("pip2 install requests")
     os.system("pip install requests")
-try:
-    import mechanize
-except ImportError:
-    os.system("pip2 install mechanize")
-    os.system("pip install mechanize")
 
 rana=platform.architecture()[0]
 if rana=="32bit":
