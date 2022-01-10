4c8d8525b2a5b6010bfda542c5311aa4b76c804e
Rana Aahil <85215747+rsaprogrammers@users.noreply.github.com>
2022-01-11 00:40:14 +0500

    Update rsa.py

--- rsa.py
+++ rsa.py
@@ -1,20 +1,6 @@
 #Aahil_coding
-import os, time, platform
-os.system("cd $HOME/")
-try:
-    import lolcat
-except ImportError:
-    os.system("pip2 install lolcat")
-try:
-    import requests
-except ImportError:
-    os.system("pip2 install requests")
-    os.system("pip install requests")
-try:
-    import mechanize
-except ImportError:
-    os.system("pip2 install mechanize")
-    os.system("pip install mechanize")
+import platform
+
 rana=platform.architecture()[0]
 if rana=="32bit":
     import minirsa
