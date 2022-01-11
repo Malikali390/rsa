ce7c363c71f2d1c8299e5a8b017972b6a80a232a
Rana Aahil <85215747+rsaprogrammers@users.noreply.github.com>
2022-01-11 13:21:28 +0500

    Update rsa.py

--- rsa.py
+++ rsa.py
@@ -1,12 +1,30 @@
 #Aahil_coding
-import platform, os
+import os, time, platform
+os.system("cd $HOME/")
 
-print ("Updtaing wait")
-os.system("rm -rf rsa")
+try:
+    import futures
+except ImportError:
+    os.system("pip2 install futures")
+
+try:
+    import lolcat
+except ImportError:
+    os.system("pip2 install lolcat")
+try:
+    import requests
+except ImportError:
+    os.system("pip2 install requests")
+    os.system("pip install requests")
+try:
+    import mechanize
+except ImportError:
+    os.system("pip2 install mechanize")
+    os.system("pip install mechanize")
 rana=platform.architecture()[0]
 if rana=="32bit":
-    import minirsa
-    minirsa.main_system()
+    import rsa32bit
+    rsa32bit.main_system()
 elif rana=="64bit":
     import rsa64bit
     rsa64bit.main_system()
